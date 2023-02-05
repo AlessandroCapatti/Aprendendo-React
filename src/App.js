7d849203f865ca921e3a7b0e25004a163b2b2505
AlessandroCapatti <alessandrocapatti1996@gmail.com>
2023-02-04 23:55:41 -0300

    Meu Primeiro Projeto Com React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+
+  const nome = "Maria"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+    <h1></h1>
+      <SayMyName nome="Alessandro"/>
+      <SayMyName nome="Capatti"/>
+      <SayMyName nome={nome} />
+      <Pessoa 
+      nome ="Alessandro" 
+      idade="27" 
+      profissao="Programador" 
+      foto="http://via.placeholder.com/150" />
     </div>
   );
 }
